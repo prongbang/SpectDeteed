1c335ed0f4143dffdd467f1cc9d7c0212f99e03d
prongbang <prongbang@gmail.com>
2019-07-20 23:05:43 +0700

    Initial Commit

---
+++ SpectDeteed/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SpectDeteed
+//
+//  Created by Endtry on 20/7/2562 BE.
+//  Copyright © 2562 Endtry. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
